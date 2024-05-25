5096b3b41febfbd3665d1013e8f8807bf0e6aab9
Said Maadan <saidmaadan@gmail.com>
2024-05-25 13:32:09 -0500

    Modified webhook/clerk/route.ts

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,8 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+
+import { WebhookEvent } from '@clerk/nextjs/server'
 import { clerkClient } from '@clerk/nextjs'
-import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
