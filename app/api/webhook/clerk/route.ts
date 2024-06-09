50947fcff75dc9cddb8e1959667f192fb1ea1ef6
Said Maadan <saidmaadan@gmail.com>
2024-06-08 23:36:33 -0500

    Updated next.config

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -4,6 +4,8 @@ import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
+
+  
 export async function POST(req: Request) {
 
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -53,15 +55,16 @@ export async function POST(req: Request) {
   // For this guide, you simply log the payload to the console
   const { id } = evt.data;
   const eventType = evt.type;
-  if(eventType === 'user.created') {
+    if (eventType === 'user.created') {
+      
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name!,
-      lastName: last_name!,
+      firstName: first_name,
+      lastName: last_name,
       photo: image_url,
     }
 
@@ -84,7 +87,7 @@ export async function POST(req: Request) {
     const user = {
       firstName: first_name,
       lastName: last_name,
-      username: username!,
+      username: username,
       photo: image_url,
     }
 
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -57,17 +57,7 @@ export async function deleteUser(clerkId: string) {
       throw new Error('User not found')
     }
 
-    // Unlink relationships
-    await Promise.all([
-      // Update the 'events' collection to remove references to the user
-      Event.updateMany(
-        { _id: { $in: userToDelete.events } },
-        { $pull: { organizer: userToDelete._id } }
-      ),
-
-      // Update the 'orders' collection to remove references to the user
-      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
-    ])
+    
 
     // Delete user
     const deletedUser = await User.findByIdAndDelete(userToDelete._id)
